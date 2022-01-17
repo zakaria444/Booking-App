23ca239f5a30cd8eb269d950f9a8d1d086bfe44f
zakaria444 <elismailizakariae@gmail.com>
2022-01-17 16:27:51 +0100

    connection with database

--- startup/db.js
+++ startup/db.js
@@ -2,10 +2,13 @@
 const mongoose = require('mongoose');
 
 module.exports = ()=>{
-    mongoose.connect(' mongodb://127.0.0.1:27017/booking    ',{
+    mongoose.connect(' mongodb://127.0.0.1:27017/booking',{
         useNewUrlParser : true,
         usefindAndModify:true,
         useUnifiedTopology:true,
         useCreateIndex:true
-    })
+    },(err)=>{
+        if(!err){console.log('MongoDB connection succeeded ')
+    }else{console.log('error in Database :'+err)}
+    });
 }
