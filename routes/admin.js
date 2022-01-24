ef369e24cf8376d3463abf50535c7ce4a059623a
zakaria444 <elismailizakariae@gmail.com>
2022-01-24 14:40:17 +0100

    Authorization JWT

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const admin =require('../models/admin.model')
-
 const router=express.Router()
+
+const dotenv = require('dotenv');
+dotenv.config({path:'config.env'})
+const JWTT = process.env.JWTT
+const JWTT2 = process.env.JWTT2
+
+
+const JWT =require('jsonwebtoken')
+
 
 //get all admin
 router.get('/',async (req,res)=>{
@@ -35,13 +43,20 @@ router.post('/add',async (req,res)=>{
     })
 
 
-    router.post('/login',async (req,res)=>{
+    router.get('/login',async (req,res)=>{
         try{
             const newadmin =await admin.findOne({email:req.body.email});
             // res.json(newadmin)
             if(newadmin.password==req.body.password){
-                
-                res.json("Login Succeeded")
+                const token = JWT.sign(
+                    { user: newadmin.email,},
+                    process.env.JWTT,
+                    {
+                      expiresIn: "2h",
+                    }
+                  );
+                  tokenn=token;
+                res.json(tokenn)
             }else{
                 res.status(401).json("login Incorect")//No autorisi
             }
@@ -50,5 +65,35 @@ router.post('/add',async (req,res)=>{
             res.status(400).json({message:err.message})//Bad request
         }
         })
+
+
+        router.get('/auth',async (req,res)=>{
+            try{
+                const token = req.body.token;
+                // res.json(newadmin)
+                if (token) {
+  
+                    // Verify the token using jwt.verify method
+                    const decode = JWT.verify(token, JWTT);
+                  
+                    //  Return response with decode data
+                    res.json({
+                      login: true,
+                      data: decode,
+                    });
+                  }else{
+                  
+                    // Return response with error
+                    res.json({
+                      login: false,
+                      data: "error",
+                    });
+                }
+                
+        
+            }catch(err){
+                res.status(400).json({message:err.message})//Bad request
+            }
+            })
 
 module.exports =router
